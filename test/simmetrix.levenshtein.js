56d46e7c22d41c2d61bfef760b6fb594fc2ce5d0
L-Ray <lennart_reuther@web.de>
2014-01-17 19:03:00 +0000

    using module structure

--- test/simmetrix.levenshtein.js
+++ test/simmetrix.levenshtein.js
@@ -1,15 +1,15 @@
-require('../lib/simmetrix.levenshtein')
+var levenshtein = require('../lib/simmetrix.levenshtein')
 var expect = require('expect.js');
 
 describe('Simmetrix.Levenshtein', function(){
   describe('#similarity()', function(){
     it('should return a proper similarity between 0 and 1', function(){
-      expect(Simmetrix.Levenshtein.similarity("Test String1", "Test String2").toFixed(4)).to.be.equal('0.9167');
+      expect(levenshtein.similarity("Test String1", "Test String2").toFixed(4)).to.be.equal('0.9167');
     }),
     it('should return 0 with undefined or empty strings', function(){
-      expect(Simmetrix.Levenshtein.similarity("", "Test String2").toFixed(4)).to.be.equal('0.0000');
-      expect(Simmetrix.Levenshtein.similarity(undefined, "Test String2").toFixed(4)).to.be.equal('0.0000');
-      expect(Simmetrix.Levenshtein.similarity("Test String1", undefined).toFixed(4)).to.be.equal('0.0000');
+      expect(levenshtein.similarity("", "Test String2").toFixed(4)).to.be.equal('0.0000');
+      expect(levenshtein.similarity(undefined, "Test String2").toFixed(4)).to.be.equal('0.0000');
+      expect(levenshtein.similarity("Test String1", undefined).toFixed(4)).to.be.equal('0.0000');
     })
   })
 })
